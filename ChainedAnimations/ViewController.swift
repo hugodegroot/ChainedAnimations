f76cf2ca8949346cafa596d61e7e67311ef579bf
Hugo de Groot <hugodegroot97@gmail.com>
2019-06-17 02:37:34 +0200

    Initial Commit

---
+++ ChainedAnimations/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChainedAnimations
+//
+//  Created by Hugo de Groot on 17/06/2019.
+//  Copyright © 2019 Hugo de Groot. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
